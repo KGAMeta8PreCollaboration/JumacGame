17174995c05ab81cb65363e83e1085b72f152e89
yugd1211 <yugd1211@gmail.com>
2025-01-06 16:13:47 +0900

    [FEAT] : Player firebase에 위치 동기화 테스트 기능 구현

---
+++ Project/Assets/03.Code/Scripts/Lobby/LobbyTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Firebase.Auth;
+using Firebase.Database;
+
+public class LobbyTest : MonoBehaviour
+{
+	private FirebaseAuth _auth;
+	private FirebaseDatabase _db;
+
+
+	private void Start()
+	{
+		_auth = FirebaseAuth.DefaultInstance;
+		_db = FirebaseDatabase.DefaultInstance;
+		_auth.CreateUserWithEmailAndPasswordAsync("bbb@bbb.bbb", "bbbbbb");
+	}
+}
