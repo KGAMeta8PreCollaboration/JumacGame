e5dca1b53b1ee858814b30797d86db6c44632971
l97038789 <l97038789@gmail.com>
2025-01-13 16:38:42 +0900

    [FEAT] : 끝선 가면 유저 데이터베이스에 돈 적립 구현

--- Project/Assets/03.Code/Scripts/LogInSystem/LogInUserData.cs
+++ Project/Assets/03.Code/Scripts/LogInSystem/LogInUserData.cs
@@ -6,22 +6,24 @@ using UnityEngine;
 [System.Serializable]
 public class LogInUserData
 {
-	public string id;
-	public string timestamp;
-	public string nickname;
-	public string race;
-	public string serverName;
-	public bool setNicknameRace;
+    public string id;
+    public string timestamp;
+    public string nickname;
+    public string race;
+    public string serverName;
+    public string money;
+    public bool setNicknameRace;
 
-	public LogInUserData(string id, string timestamp = default, string nickname = default, string race = default, string serverName = default, bool setNicknameRace = false)
-	{
-		this.id = id;
-		this.timestamp = timestamp;
-		this.nickname = nickname;
-		this.race = race;
-		this.serverName = serverName;
-		this.setNicknameRace = setNicknameRace;
-	}
+    public LogInUserData(string id, string timestamp = default, string nickname = default, string race = default, string serverName = default, string money = default, bool setNicknameRace = false)
+    {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.nickname = nickname;
+        this.race = race;
+        this.serverName = serverName;
+        this.money = money;
+        this.setNicknameRace = setNicknameRace;
+    }
 
-	public LogInUserData() { }
+    public LogInUserData() { }
 }
--- Project/Assets/03.Code/Scripts/RGLightGame/RGLightManager.cs
+++ Project/Assets/03.Code/Scripts/RGLightGame/RGLightManager.cs
@@ -1,3 +1,6 @@
+using Firebase.Database;
+using Newtonsoft.Json;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +15,7 @@ namespace Minigame.RGLight
         [SerializeField] private GameObject _introPanel;
         [SerializeField] private Minigame.RGLight.Player _playerPrefab;
         [SerializeField] private Transform _startPoint;
+        public int defaultMoney;
 
 
 
@@ -54,12 +58,43 @@ namespace Minigame.RGLight
 
         private void OnSuccess()
         {
+            SetMoney(defaultMoney);
             PopupManager.Instance.PopupOpen<AlarmPopup>().SetPopup("성공", "컨트롤이 좋노", EndGame);
         }
 
         private void OnDefeat()
         {
+            SetMoney(defaultMoney);
             PopupManager.Instance.PopupOpen<AlarmPopup>().SetPopup("실패", "컨트롤이 안좋노", EndGame);
+        }
+
+        private async void SetMoney(int value)
+        {
+            try
+            {
+                DatabaseReference logInUsers = GameManager.Instance.FirebaseManager.LogInUsersRef;
+                DataSnapshot snapshot = await logInUsers.Child(GameManager.Instance.FirebaseManager.User.UserId).GetValueAsync();
+                LogInUserData userData;
+
+                if (snapshot.Exists)
+                {
+                    userData = JsonConvert.DeserializeObject<LogInUserData>(snapshot.GetRawJsonValue());
+                    int money = value;
+                    if (int.TryParse(userData.money, out int result)) money += result;
+                    userData.money = money.ToString();
+                }
+                else
+                {
+                    userData = new LogInUserData(GameManager.Instance.FirebaseManager.User.UserId, money: value.ToString());
+                }
+
+                string json = JsonConvert.SerializeObject(userData);
+                await logInUsers.Child(GameManager.Instance.FirebaseManager.User.UserId).SetRawJsonValueAsync(json);
+            }
+            catch (Exception e)
+            {
+                print(e.Message);
+            }
         }
     }
 }
