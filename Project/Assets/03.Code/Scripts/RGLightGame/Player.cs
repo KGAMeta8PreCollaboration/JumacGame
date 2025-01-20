ffb3d9ca4df3ef63cdec5005c099c208b1874d61
LeeSeoHyeong1031 <champ36912@naver.com>
2025-01-21 00:54:46 +0900

    [FEAT] : 사용자 캐릭터로 플레이 할 수 있게 구현

--- Project/Assets/03.Code/Scripts/RGLightGame/Player.cs
+++ Project/Assets/03.Code/Scripts/RGLightGame/Player.cs
@@ -5,74 +5,97 @@ using UnityEngine;
 
 namespace Minigame.RGLight
 {
-    public class Player : MonoBehaviour
-    {
-        public float moveSpeed;
-        public float maxHealth;
-        private float _curHealth;
+	public class Player : MonoBehaviour
+	{
+		public float moveSpeed;
+		public float maxHealth;
+		private float _curHealth;
 
-        private MainPage _mainPage;
+		private MainPage _mainPage;
+		[SerializeField] private List<GameObject> _models = new List<GameObject>();
 
-        public float HealthAmount
-        {
-            get
-            {
-                return _curHealth / maxHealth;
-            }
-        }
-        public bool IsDead { get; private set; }
+		public float HealthAmount
+		{
+			get
+			{
+				return _curHealth / maxHealth;
+			}
+		}
+		public bool IsDead { get; private set; }
 
-        public RGLightManager RGLightManager { get; private set; }
+		public RGLightManager RGLightManager { get; private set; }
 
-        public PlayerDistanceTracker PlayerDistanceTracker { get; private set; }
-        public PlayerRay PlayerRay { get; private set; }
-        private PlayerInputManager _playerInputManager;
-        private Rigidbody _playerRigidbody;
+		public PlayerDistanceTracker PlayerDistanceTracker { get; private set; }
+		public PlayerRay PlayerRay { get; private set; }
+		private PlayerInputManager _playerInputManager;
+		private Rigidbody _playerRigidbody;
+		private Animator _playerAnimator;
 
-        private void Update()
-        {
-            _mainPage.SetHealth(HealthAmount);
-        }
+		private void Update()
+		{
+			_mainPage.SetHealth(HealthAmount);
+		}
 
-        private void FixedUpdate()
-        {
-            if (RGLightManager.IsEndGame) return;
-            Move(_playerInputManager.InputMoveDir);
-        }
+		private void FixedUpdate()
+		{
+			if (RGLightManager.IsEndGame) return;
 
-        private void Move(Vector2 input)
-        {
-            Vector3 actualMove = new Vector3(input.x, 0, input.y).normalized * moveSpeed * Time.fixedDeltaTime;
-            _playerRigidbody.MovePosition(_playerRigidbody.position + actualMove);
-        }
+			_playerAnimator.SetBool("Walk", _playerInputManager.InputMoveDir.magnitude >= 0.1f);
 
-        public void TakeDamage(float damage)
-        {
-            _curHealth -= damage;
-            if (_curHealth <= 0 && !IsDead) Die();
-        }
+			Move(_playerInputManager.InputMoveDir);
+		}
 
-        private void Die()
-        {
-            IsDead = true;
-            _curHealth = 0;
-            RGLightManager.GameResult(false);
-        }
+		private void Move(Vector2 input)
+		{
+			Vector3 actualMove = new Vector3(input.x, 0, input.y).normalized * moveSpeed * Time.fixedDeltaTime;
+			_playerRigidbody.MovePosition(_playerRigidbody.position + actualMove);
+		}
 
-        public void Init(RGLightManager manager)
-        {
-            _playerInputManager = GetComponent<PlayerInputManager>();
-            _playerRigidbody = GetComponent<Rigidbody>();
-            PlayerDistanceTracker = GetComponent<PlayerDistanceTracker>();
-            PlayerRay = GetComponent<PlayerRay>();
-            _mainPage = GameObject.FindObjectOfType<MainPage>();
+		public void TakeDamage(float damage)
+		{
+			_curHealth -= damage;
+			if (_curHealth <= 0 && !IsDead) Die();
+		}
 
-            RGLightManager = manager;
-            CinemachineVirtualCamera cvc = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
-            cvc.Follow = transform;
-            cvc.LookAt = transform;
+		private void Die()
+		{
+			IsDead = true;
+			_curHealth = 0;
+			RGLightManager.GameResult(false);
+		}
 
-            _curHealth = maxHealth;
-        }
-    }
+		public async void Init(RGLightManager manager)
+		{
+			_playerInputManager = GetComponent<PlayerInputManager>();
+			_playerRigidbody = GetComponent<Rigidbody>();
+			PlayerDistanceTracker = GetComponent<PlayerDistanceTracker>();
+			PlayerRay = GetComponent<PlayerRay>();
+			_mainPage = GameObject.FindObjectOfType<MainPage>();
+			_playerAnimator = GetComponentInChildren<Animator>();
+
+			RGLightManager = manager;
+
+			string race = await RGLightManager.GetRace();
+			if (race != null)
+			{
+				foreach (GameObject model in _models)
+				{
+					if (model.name.Equals(race))
+					{
+						model.SetActive(true);
+					}
+					else
+					{
+						model.SetActive(false);
+					}
+				}
+			}
+
+			CinemachineVirtualCamera cvc = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+			cvc.Follow = transform;
+			cvc.LookAt = transform;
+
+			_curHealth = maxHealth;
+		}
+	}
 }
--- Project/Assets/03.Code/Scripts/RGLightGame/Younghee/FireWave.cs
+++ Project/Assets/03.Code/Scripts/RGLightGame/Younghee/FireWave.cs
@@ -5,91 +5,91 @@ using System;
 
 public class FireWave : Skill
 {
-    [Header("Settings")]
-    public float squareGrowTime = 1f;
-    public float squareMaxHeight = 5.7f;
-    public float squareWidth = 1f;
-    public float skillInterval = 0.5f;
+	[Header("Settings")]
+	public float squareGrowTime = 1f;
+	public float squareMaxHeight = 5.7f;
+	public float squareWidth = 1f;
+	public float skillInterval = 0.5f;
 
-    [Header("Prefabs")]
-    [SerializeField] private GameObject _squarePrefab;
-    [SerializeField] private GameObject _effectPrefab;
+	[Header("Prefabs")]
+	[SerializeField] private GameObject _squarePrefab;
+	[SerializeField] private GameObject _effectPrefab;
 
-    private Younghee _younghee;
+	private Younghee _younghee;
 
-    public override void Init(Younghee younghee)
-    {
-        _younghee = younghee;
-    }
+	public override void Init(Younghee younghee)
+	{
+		_younghee = younghee;
+	}
 
-    public override void UseSkill()
-    {
-        List<Vector3> allCellCenters = _younghee.RGLightManager.CageManager.cage.GetCellCenters();
+	public override void UseSkill()
+	{
+		List<Vector3> allCellCenters = _younghee.RGLightManager.CageManager.cage.GetCellCenters();
 
-        List<Vector3> outerCellCenters = _younghee.RGLightManager.CageManager.cage.GetOuterCellCenters();
+		List<Vector3> outerCellCenters = _younghee.RGLightManager.CageManager.cage.GetOuterCellCenters();
 
-        float leftX = _younghee.RGLightManager.CageManager.cage.transform.position.x -
-                      _younghee.RGLightManager.CageManager.cage.width / 2f;
-        List<Vector3> leftPoints = outerCellCenters.FindAll(point => Mathf.Approximately(point.x, leftX));
+		float leftX = _younghee.RGLightManager.CageManager.cage.transform.position.x -
+					  _younghee.RGLightManager.CageManager.cage.width / 2f;
+		List<Vector3> leftPoints = outerCellCenters.FindAll(point => Mathf.Approximately(point.x, leftX));
 
-        Vector3 startPoint = leftPoints[UnityEngine.Random.Range(0, leftPoints.Count)];
+		Vector3 startPoint = leftPoints[UnityEngine.Random.Range(0, leftPoints.Count)];
 
-        List<Vector3> wavePath = allCellCenters.FindAll(point =>
-            Mathf.Approximately(point.z, startPoint.z));
-        wavePath.Sort((a, b) => a.x.CompareTo(b.x));
+		List<Vector3> wavePath = allCellCenters.FindAll(point =>
+			Mathf.Approximately(point.z, startPoint.z));
+		wavePath.Sort((a, b) => a.x.CompareTo(b.x));
 
-        StartCoroutine(ExecuteSkillInWave(startPoint, wavePath));
-    }
+		StartCoroutine(ExecuteSkillInWave(startPoint, wavePath));
+	}
 
-    private IEnumerator ExecuteSkillInWave(Vector3 startPoint, List<Vector3> wavePath)
-    {
-        Quaternion rotation = Quaternion.Euler(90f, 0f, 0f);
-        GameObject square = Instantiate(_squarePrefab, startPoint, rotation);
+	private IEnumerator ExecuteSkillInWave(Vector3 startPoint, List<Vector3> wavePath)
+	{
+		Quaternion rotation = Quaternion.Euler(90f, 0f, 0f);
+		GameObject square = Instantiate(_squarePrefab, startPoint, rotation);
 
-        yield return StartCoroutine(ExpandSquare(square, squareWidth, squareMaxHeight, squareGrowTime));
+		yield return StartCoroutine(ExpandSquare(square, squareWidth, squareMaxHeight, squareGrowTime));
 
-        Destroy(square);
+		Destroy(square);
 
-        List<GameObject> curEffects = new List<GameObject>();
+		List<GameObject> curEffects = new List<GameObject>();
 
-        foreach (Vector3 point in wavePath)
-        {
-            GameObject effect = Instantiate(_effectPrefab, point, Quaternion.identity);
-            curEffects.Add(effect);
-            yield return new WaitForSeconds(skillInterval);
-        }
+		foreach (Vector3 point in wavePath)
+		{
+			GameObject effect = Instantiate(_effectPrefab, point, Quaternion.identity);
+			curEffects.Add(effect);
+			yield return new WaitForSeconds(skillInterval);
+		}
 
-        yield return new WaitForSeconds(3f);
+		yield return new WaitForSeconds(2.5f);
 
-        foreach (GameObject effect in curEffects)
-        {
-            Destroy(effect);
-        }
+		foreach (GameObject effect in curEffects)
+		{
+			Destroy(effect);
+		}
 
-        print("FireWave 이펙트 삭제");
-        _younghee.skillDone.Add(true);
-    }
+		print("FireWave 이펙트 삭제");
+		_younghee.skillDone.Add(true);
+	}
 
-    private IEnumerator ExpandSquare(GameObject square, float squareWidth, float squareMaxHeight, float squareGrowTime)
-    {
-        square.transform.localScale = new Vector3(squareWidth, 0f, 1f);
-        Vector3 maxSize = new Vector3(squareMaxHeight, squareWidth, 1f);
+	private IEnumerator ExpandSquare(GameObject square, float squareWidth, float squareMaxHeight, float squareGrowTime)
+	{
+		square.transform.localScale = new Vector3(squareWidth, 0f, 1f);
+		Vector3 maxSize = new Vector3(squareMaxHeight, squareWidth, 1f);
 
-        float elapsedTime = 0f;
+		float elapsedTime = 0f;
 
-        while (elapsedTime < squareGrowTime)
-        {
-            elapsedTime += Time.deltaTime;
+		while (elapsedTime < squareGrowTime)
+		{
+			elapsedTime += Time.deltaTime;
 
-            float progress = Mathf.Clamp01(elapsedTime / squareGrowTime);
-            float newHeight = Mathf.Lerp(0f, squareMaxHeight, progress);
-            square.transform.localScale = new Vector3(newHeight, squareWidth, 1f);
+			float progress = Mathf.Clamp01(elapsedTime / squareGrowTime);
+			float newHeight = Mathf.Lerp(0f, squareMaxHeight, progress);
+			square.transform.localScale = new Vector3(newHeight, squareWidth, 1f);
 
-            yield return null;
-        }
+			yield return null;
+		}
 
-        square.transform.localScale = maxSize;
+		square.transform.localScale = maxSize;
 
-        yield return new WaitForSeconds(1f);
-    }
+		yield return new WaitForSeconds(0.5f);
+	}
 }
