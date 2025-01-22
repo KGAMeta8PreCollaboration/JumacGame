cb826497299745510c97b90837bfe82066646759
l97038789 <l97038789@gmail.com>
2025-01-22 12:06:49 +0900

    [STYLE] : 무궁화 미니게임 플레이어 모바일 조이스틱으로 움직이도록 코드 변경

--- Project/Assets/03.Code/Scripts/RGLightGame/Player.cs
+++ Project/Assets/03.Code/Scripts/RGLightGame/Player.cs
@@ -2,100 +2,116 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Minigame.RGLight
 {
-	public class Player : MonoBehaviour
-	{
-		public float moveSpeed;
-		public float maxHealth;
-		private float _curHealth;
+    public class Player : MonoBehaviour
+    {
+        public float moveSpeed;
+        public float rotateSpeed;
 
-		private MainPage _mainPage;
-		[SerializeField] private List<GameObject> _models = new List<GameObject>();
+        public float maxHealth;
+        private float _curHealth;
 
-		public float HealthAmount
-		{
-			get
-			{
-				return _curHealth / maxHealth;
-			}
-		}
-		public bool IsDead { get; private set; }
+        private MainPage _mainPage;
+        [SerializeField] private List<GameObject> _models = new List<GameObject>();
 
-		public RGLightManager RGLightManager { get; private set; }
+        public float HealthAmount
+        {
+            get
+            {
+                return _curHealth / maxHealth;
+            }
+        }
+        public bool IsDead { get; private set; }
 
-		public PlayerDistanceTracker PlayerDistanceTracker { get; private set; }
-		public PlayerRay PlayerRay { get; private set; }
-		private PlayerInputManager _playerInputManager;
-		private Rigidbody _playerRigidbody;
-		private Animator _playerAnimator;
+        public RGLightManager RGLightManager { get; private set; }
 
-		private void Update()
-		{
-			_mainPage.SetHealth(HealthAmount);
-		}
+        public PlayerDistanceTracker PlayerDistanceTracker { get; private set; }
+        public PlayerRay PlayerRay { get; private set; }
+        private PlayerInputManager _playerInputManager;
+        private Rigidbody _playerRigidbody;
+        private Animator _playerAnimator;
+        private Joystick _joystick;
 
-		private void FixedUpdate()
-		{
-			if (RGLightManager.IsEndGame) return;
+        private void Update()
+        {
+            _mainPage.SetHealth(HealthAmount);
+        }
 
-			_playerAnimator.SetBool("Walk", _playerInputManager.InputMoveDir.magnitude >= 0.1f);
+        private void FixedUpdate()
+        {
+            if (RGLightManager.IsEndGame) return;
 
-			Move(_playerInputManager.InputMoveDir);
-		}
+            _playerAnimator.SetBool("Walk", _playerInputManager.InputMoveDir.magnitude >= 0.1f);
 
-		private void Move(Vector2 input)
-		{
-			Vector3 actualMove = new Vector3(input.x, 0, input.y).normalized * moveSpeed * Time.fixedDeltaTime;
-			_playerRigidbody.MovePosition(_playerRigidbody.position + actualMove);
-		}
+            Vector3 inputDir = new Vector3(-_joystick.dir.x, 0, -_joystick.dir.y);
+            Move(inputDir);
+            Rotate(inputDir);
+        }
 
-		public void TakeDamage(float damage)
-		{
-			_curHealth -= damage;
-			if (_curHealth <= 0 && !IsDead) Die();
-		}
+        private void Move(Vector3 input)
+        {
+            Vector3 actualMove = new Vector3(input.x, 0, input.z).normalized * moveSpeed * Time.fixedDeltaTime;
+            _playerRigidbody.MovePosition(_playerRigidbody.position + actualMove);
+        }
 
-		private void Die()
-		{
-			IsDead = true;
-			_curHealth = 0;
-			RGLightManager.GameResult(false);
-		}
+        public void Rotate(Vector3 input)
+        {
+            if (input.sqrMagnitude < 0.1f) return;
 
-		public async void Init(RGLightManager manager)
-		{
-			_playerInputManager = GetComponent<PlayerInputManager>();
-			_playerRigidbody = GetComponent<Rigidbody>();
-			PlayerDistanceTracker = GetComponent<PlayerDistanceTracker>();
-			PlayerRay = GetComponent<PlayerRay>();
-			_mainPage = GameObject.FindObjectOfType<MainPage>();
-			_playerAnimator = GetComponentInChildren<Animator>();
+            Vector3 direction = new Vector3(input.x, 0, input.z);
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.fixedDeltaTime);
+        }
 
-			RGLightManager = manager;
+        public void TakeDamage(float damage)
+        {
+            _curHealth -= damage;
+            if (_curHealth <= 0 && !IsDead) Die();
+        }
 
-			string race = await RGLightManager.GetRace();
-			if (race != null)
-			{
-				foreach (GameObject model in _models)
-				{
-					if (model.name.Equals(race))
-					{
-						model.SetActive(true);
-					}
-					else
-					{
-						model.SetActive(false);
-					}
-				}
-			}
+        private void Die()
+        {
+            IsDead = true;
+            _curHealth = 0;
+            RGLightManager.GameResult(false);
+        }
 
-			CinemachineVirtualCamera cvc = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
-			cvc.Follow = transform;
-			cvc.LookAt = transform;
+        public async void Init(RGLightManager manager)
+        {
+            _playerInputManager = GetComponent<PlayerInputManager>();
+            _playerRigidbody = GetComponent<Rigidbody>();
+            PlayerDistanceTracker = GetComponent<PlayerDistanceTracker>();
+            PlayerRay = GetComponent<PlayerRay>();
+            _mainPage = GameObject.FindObjectOfType<MainPage>();
+            _playerAnimator = GetComponentInChildren<Animator>();
+            _joystick = GameObject.FindObjectOfType<Joystick>();
 
-			_curHealth = maxHealth;
-		}
-	}
+            RGLightManager = manager;
+
+            string race = await RGLightManager.GetRace();
+            if (race != null)
+            {
+                foreach (GameObject model in _models)
+                {
+                    if (model.name.Equals(race))
+                    {
+                        model.SetActive(true);
+                    }
+                    else
+                    {
+                        model.SetActive(false);
+                    }
+                }
+            }
+
+            CinemachineVirtualCamera cvc = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+            cvc.Follow = transform;
+            cvc.LookAt = transform;
+
+            _curHealth = maxHealth;
+        }
+    }
 }
