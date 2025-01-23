d0c25cb8af4f02d923de3f8dfdf74b92b982e44b
l97038789 <l97038789@gmail.com>
2025-01-23 13:42:12 +0900

    [ENV] : 사운드 에셋 사용 용도에 맞춰 세팅

--- Project/Assets/03.Code/Scripts/UI/Popup/SoundSettingsPopup.cs
+++ Project/Assets/03.Code/Scripts/UI/Popup/SoundSettingsPopup.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
@@ -8,61 +9,67 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class SoundSettingsPopup : Popup
 {
-	[SerializeField] private AudioMixer _audioMixer;
-	[SerializeField] private Slider _bgmSlider;
-	[SerializeField] private Slider _sfxSlider;
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _bgmSlider;
+    [SerializeField] private Slider _sfxSlider;
+    [SerializeField] private TextMeshProUGUI _bgmSoundText;
+    [SerializeField] private TextMeshProUGUI _sfxSoundText;
 
-	private void Awake()
-	{
-		_bgmSlider.onValueChanged.AddListener(SetBgmVolume);
-		_sfxSlider.onValueChanged.AddListener(SetSfxVolume);
-	}
+    private void Awake()
+    {
+        _bgmSlider.onValueChanged.AddListener(SetBgmVolume);
+        _sfxSlider.onValueChanged.AddListener(SetSfxVolume);
+    }
 
-	public void InitVolume()
-	{
-		SetBgmVolume(_bgmSlider.value);
-		SetSfxVolume(_sfxSlider.value);
-	}
+    public void InitVolume()
+    {
+        SetBgmVolume(_bgmSlider.value);
+        SetSfxVolume(_sfxSlider.value);
+    }
 
-	private void SetBgmVolume(float value)
-	{
-		_audioMixer.SetFloat("bgm", Mathf.Log10(value) * 20);
-		PlayerPrefs.SetFloat("bgm", _bgmSlider.value);
-	}
+    private void SetBgmVolume(float value)
+    {
+        int valueInt = Mathf.RoundToInt(value * 100);
+        _bgmSoundText.text = valueInt.ToString();
+        _audioMixer.SetFloat("bgm", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("bgm", _bgmSlider.value);
+    }
 
-	private void SetSfxVolume(float value)
-	{
-		_audioMixer.SetFloat("sfx", Mathf.Log10(value) * 20);
-		PlayerPrefs.SetFloat("sfx", _sfxSlider.value);
-	}
+    private void SetSfxVolume(float value)
+    {
+        int valueInt = Mathf.RoundToInt(value * 100);
+        _sfxSoundText.text = valueInt.ToString();
+        _audioMixer.SetFloat("sfx", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("sfx", _sfxSlider.value);
+    }
 
-	public bool CanLoadVolume()
-	{
-		if (PlayerPrefs.HasKey("bgm") && PlayerPrefs.HasKey("sfx"))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    public bool CanLoadVolume()
+    {
+        if (PlayerPrefs.HasKey("bgm") && PlayerPrefs.HasKey("sfx"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-	public void LoadVolume()
-	{
-		float bgmV = PlayerPrefs.GetFloat("bgm");
-		float sfxV = PlayerPrefs.GetFloat("sfx");
+    public void LoadVolume()
+    {
+        float bgmV = PlayerPrefs.GetFloat("bgm");
+        float sfxV = PlayerPrefs.GetFloat("sfx");
 
-		_bgmSlider.value = bgmV;
-		_sfxSlider.value = sfxV;
+        _bgmSlider.value = bgmV;
+        _sfxSlider.value = sfxV;
 
-		SetBgmVolume(bgmV);
-		SetSfxVolume(sfxV);
-	}
+        SetBgmVolume(bgmV);
+        SetSfxVolume(sfxV);
+    }
 
-	private void OnDestroy()
-	{
-		_bgmSlider.onValueChanged.RemoveListener(SetBgmVolume);
-		_sfxSlider.onValueChanged.RemoveListener(SetSfxVolume);
-	}
+    private void OnDestroy()
+    {
+        _bgmSlider.onValueChanged.RemoveListener(SetBgmVolume);
+        _sfxSlider.onValueChanged.RemoveListener(SetSfxVolume);
+    }
 }
